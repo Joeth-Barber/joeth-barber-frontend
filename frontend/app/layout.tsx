d0b19e7294f4f78678258457fa338fe6b1f638b9
doguebart <douglasweelber.2004@outlook.com>
2024-02-17 19:29:10 -0300

    chore: add shadcn setup

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Joeth Baber",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${outfit.className} dark`}>{children}</body>
+    </html>
+  );
+}
