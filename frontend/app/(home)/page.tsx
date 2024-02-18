b18d6163473c4befa6f441b3ad5b7bf04e84beb2
doguebart <douglasweelber.2004@outlook.com>
2024-02-18 20:47:50 -0300

    style: change background color

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,15 +1,17 @@
 import ClientesSection from "./_components/clientes";
 import GaleriaFotos from "./_components/galeria";
 import HeroSection from "./_components/hero";
+import ReservaSection from "./_components/reserva";
 import Services from "./_components/services";
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-dark">
       <HeroSection />
       <Services />
       <GaleriaFotos />
       <ClientesSection />
+      <ReservaSection />
     </div>
   );
 };
